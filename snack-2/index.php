12ace53c70f400c3ce9c56c4d003b9ffcbcc2cf0
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-06-22 14:04:13 +0200

    add snack 2

--- snack-2/index.php
+++ snack-2/index.php
@@ -3,5 +3,17 @@ Verificare che name sia più lungo di 3 caratteri, che mail contenga un punto e 
 Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”. -->
 
 <?php
+    // Creo le variabili per salvare i parametri GET che mi servono
+    $userName = $_GET['name'];
+    $userMail = $_GET['mail'];
+    $userAge = $_GET['age'];
 
+    // SE name è più lungo di 3 caratteri && mail contiene un punto e una chiocciola && age è un numero
+    // ALLORA stampo "Accesso riuscito"
+    // ALTRIMENTI stampo "Accesso negato"
+    if (strlen($userName) > 3 && filter_var($userMail, FILTER_VALIDATE_EMAIL) && is_numeric($userAge)) {
+        echo "Accesso riuscito";
+    } else {
+        echo "Accesso negato";
+    }
 ?>
